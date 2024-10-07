aa4805a039a5ec70605abb4d65d96c1416973ac2
Dorrrran <theo.gustafsson05@gmail.com>
2024-10-07 11:58:58 +0200

    spara bild p kolla färg c

--- spektrometer/spectrometer.py
+++ spektrometer/spectrometer.py
@@ -1,19 +1,33 @@
 import cv2
-import numpy as np
-cap = cv2.VideoCapture(1)
+import os
+import pyautogui, sys
+from pynput.mouse import Listener, Button
+path = r"C:\Users\theos\SpectroImg"
+pathIn = r"C:\Users\theos\SpectroImg\1_tQYAE9PU2FIrZV5rKwT7AQ.jpg"
+base_name = 'spektrum'
+extension = '.jpg'
+file_index = 1
+cap = cv2.VideoCapture(0)
 
 
 while True:
+    img = cv2.imread(pathIn)
+    #x, y = pyautogui.position()
     ret, frame = cap.read()
-    height, width, _ = frame.shape
-    
-    center_x, center_y = width // 2, height // 2
-    color = frame[center_y, center_x]
-    b, g, r = color
     cv2.imshow("window",frame)
     if cv2.waitKey(1) & 0xFF == ord("p"):
-        print(f'R:{r}, G: {g}, B: {b}')
-
+        while True:
+            filename = os.path.join(path, f"{base_name}{file_index}{extension}")
+            if os.path.exists(filename):
+                file_index += 1
+            else:
+                cv2.imwrite(filename, frame)
+                print(f'Saved: {filename}')
+                break
+    elif cv2.waitKey(1) & 0xFF == ord("c"):
+        color = frame[y, x]
+        b, g, r = color
+        print(f'B = {b}, G = {g}, R = {r}')
     elif cv2.waitKey(1) & 0xFF == ord("q"):
         break
 cap.release()
