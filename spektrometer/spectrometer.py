892ce95f3cb5de9e22a7f8bd2ab3385ac1b53d39
Dorrrran <theo.gustafsson05@gmail.com>
2024-10-07 12:37:24 +0200

    sad

--- spektrometer/spectrometer.py
+++ spektrometer/spectrometer.py
@@ -1,9 +1,8 @@
 import cv2
 import os
 import pyautogui, sys
-from pynput.mouse import Listener, Button
 path = r"C:\Users\theos\SpectroImg"
-pathIn = r"C:\Users\theos\SpectroImg\1_tQYAE9PU2FIrZV5rKwT7AQ.jpg"
+pathIn = r"C:\Users\theos\SpectroImg\spektrum6.jpg"
 base_name = 'spektrum'
 extension = '.jpg'
 file_index = 1
@@ -12,9 +11,10 @@ cap = cv2.VideoCapture(0)
 
 while True:
     img = cv2.imread(pathIn)
-    #x, y = pyautogui.position()
+    x, y = pyautogui.position()
     ret, frame = cap.read()
-    cv2.imshow("window",frame)
+    cv2.imshow("cam",frame)
+    cv2.imshow("Spectrum",img)
     if cv2.waitKey(1) & 0xFF == ord("p"):
         while True:
             filename = os.path.join(path, f"{base_name}{file_index}{extension}")
@@ -24,6 +24,10 @@ while True:
                 cv2.imwrite(filename, frame)
                 print(f'Saved: {filename}')
                 break
+    elif cv2.waitKey(1) & 0xFF == ord("v"):
+        color1 = img[y, x]
+        b1, g1, r1 = color1
+        print(f'B1 = {b1}, G1 = {g1}, R1 = {r1}')
     elif cv2.waitKey(1) & 0xFF == ord("c"):
         color = frame[y, x]
         b, g, r = color
