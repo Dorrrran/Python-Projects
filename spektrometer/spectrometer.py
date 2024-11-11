668e1c834ae8a53a7ed42db529a1f236dc96c6e5
Victor Hedlund <victor.s.hedlund@gmail.com>
2024-11-11 11:08:39 +0100

    kommentarer och fixat namn

--- spektrometer/spectrometer.py
+++ spektrometer/spectrometer.py
@@ -26,13 +26,22 @@ def crop_image_to_rectangle(image, top_left, bottom_right):
     cv2.imwrite(r"C:\Users\theos\SpectroImg\cropped.png" , cropped_image)
     return cropped_image
 
-#kollar på varje färg samt dess intensitet och försöker aproximera till ett spektrum
+#kollar på varje färg samt dess intensitet och försöker aproximera till ett spektrum.
 def rgb_to_wavelength(gray,gray1,gray2, pxw, w):
+
+    """
+    Dettna funktion är det som bestämmer våra våglängder.
+    Den gör det genom att sätta px 0 till lägsta våglängden och sista pixeln till högsta våglängden.
+    Funktionen kommer få olika pixelpositioner för tre rader för att säkerhetställa ett bättre resultat.
+    Medelvärdet av alla intensiteter beräknas
+    Bilden som används här en en beskärning efter ett fullt spektrum där alla våglängder syns.
+    Vi kan då veta att första och sista pixeln då motsvarar första och sista våglängden
+    """
     # Beräkna luminositet som intensitetsvärde mellan 0 och 1
     luminosity = ((gray / 255) + (gray1/255) + (gray2/255))/3
     # Omvandla pixelpositionen pxw till en våglängd
     wavelength = min_wavelength + (pxw / w) * (max_wavelength - min_wavelength)
-    WaveInt[pxw]= wavelength, luminosity
+    WaveInt[pxw]= wavelength, luminosity #Här sparas våglängden och medelintensiteten för tre rader
 
 #skapa pdf med datainsammling
 def Create_pdf(output_pdf= r"C:\Users\theos\SpectroImg"):
@@ -85,8 +94,11 @@ def CalibratedImage(image, kal_top_left, kal_bottom_right):
     return kal_cropped_image
 
 
-# hittar största mängd pixlar och skickar ut 2 (top left och bot right) koordinater för den rektangeln som innesluter detta område
-# Denna kollar på en rektangel som är lite större än området och används så att kameran kan ignorerera annat ljus som möjligtvis skulle kunna hindra nästa steg ifrån att kolla på rätt del av bilden
+"""
+hittar största mängd pixlar och skickar ut 2 (top left och bot right) 
+koordinater för den rektangeln som innesluter detta område.
+Denna kollar på en rektangel som är lite större än området och används så att kameran
+kan ignorerera annat ljus som möjligtvis skulle kunna hindra nästa steg ifrån att kolla på rätt del av bilden"""
 def CaliFrame(frame):
     kal_top_left_rect = None
     kal_bottom_right_rect = None    
@@ -102,44 +114,52 @@ def CaliFrame(frame):
    
 while True:
     ret, frame = cap.read()
-    cv2.imshow("cam",frame)
+
+    cv2.imshow("cam",frame) #ger en live feed till användaren, där man kan se allt kameran kan se. (Detta är inte Cropped Frame)!
+
+    #När man trycker på v så tar men ett mät resultat OBS! Spektrometern måste kalibreras innan
     if cv2.waitKey(1) & 0xFF == ord("v"):
-        kalibrerad = CalibratedImage(frame,kal_top_left_rect, kal_bot_right_rect)
+        Calibrated = CalibratedImage(frame,kal_top_left_rect, kal_bot_right_rect)
         cropped_image = crop_image_to_rectangle(frame, kal_top_left_rect, kal_bot_right_rect)
         #ange en färg till varje pixel och sortera ut onödiga färger
         # Reinitialize Intensitet for the cropped image
         h, w, _ = cropped_image.shape
         WaveInt = [[0, 0] for _ in range(w)]
-        #SvartVit version av bild
+        #SvartVit version av beskuren bild
         cropped_image_gray = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
         for pxWidth in range(w):
+            #kollar på raden i mitten av det som ges av gittret, Även raden under och över för säkrare resultat
             gray = cropped_image_gray[int(h/2)-1, pxWidth]
             gray1 = cropped_image_gray[int(h/2), pxWidth]
             gray2 = cropped_image_gray[int(h/2)+1, pxWidth]
-            #aproximera våglängden och intensitet
+            #aproximera våglängden och intensitet, Se funktion längre upp
             rgb_to_wavelength(gray,gray1,gray2,pxWidth,w)
 
+        #detta påverkar inte mätreslutat. Ändast för att göra grafen snyggare genom att avsluta och sätta ett startvärde till 0
         WaveInt.insert(0, (WaveInt[0][0]-1, 0))
         WaveInt.append((WaveInt[-1][0] + 1, 0)) 
-        # Skapa grafen med våglängd på x-axeln och intensitet på y-axeln
-        WaveInt_np = np.array(WaveInt) #Gör om WaveInt till 2 2d arrayer som sen kan sparas i excel fil
-        Våglängdarray = WaveInt_np[:,0]
-        Intensitetarray = WaveInt_np[:,1]
-        VåglängdDF = pd.DataFrame(Våglängdarray) # sparar våglängd och intensitet i 2 separata excel document
-        IntensitetDF = pd.DataFrame(Intensitetarray)
 
-# Remove files if they exist, then save the new versions
+        #delar upp waveInt array till två nya, en för intensitet en för våglängder. De används till grafen
+        WaveInt_np = np.array(WaveInt)
+        WaveLengthArray = WaveInt_np[:,0] 
+        IntensityArray = WaveInt_np[:,1]
+        WaveLengthDF = pd.DataFrame(WaveLengthArray) # sparar våglängd och intensitet i 2 separata excel document
+        IntensityDF = pd.DataFrame(IntensityArray)
 
+
+        # Remove files if they exist, then save the new versions
         if os.path.exists(wavelength_path) or os.path.exists(intensity_path):
             os.remove(wavelength_path)
             os.remove(intensity_path)
-        VåglängdDF.to_excel(r"C:\Users\theos\SpectroImg\Våglängder.xlsx", index=False, header=False)
-        IntensitetDF.to_excel(r"C:\Users\theos\SpectroImg\Intensitet.xlsx", index=False, header=False)
-        Intensitet_max = max(Intensitetarray)
+        WaveLengthDF.to_excel(r"C:\Users\theos\SpectroImg\Våglängder.xlsx", index=False, header=False)
+        IntensityDF.to_excel(r"C:\Users\theos\SpectroImg\Intensitet.xlsx", index=False, header=False)
+        Intensity_max = max(IntensityArray)
+
+        # Skapa grafen med våglängd på x-axeln och intensitet på y-axeln
         # Plotta den släta linjen
-        plt.xlim(350, 800)
-        plt.ylim(0, 110)
-        plt.plot(Våglängdarray , Intensitetarray/Intensitet_max * 100, '-', color="black")  # Slät kurva
+        plt.xlim(350, 800) #x axel
+        plt.ylim(0, 110)   #y axel
+        plt.plot(WaveLengthArray , IntensityArray/Intensity_max * 100, '-', color="black")  # Slät kurva
         plt.xlabel("Wavelength (nm)")
         plt.ylabel("Relativ Intensity (%)")
         plt.title("Intensity vs Wavelength")
@@ -152,8 +172,8 @@ while True:
 
     elif cv2.waitKey(1) & 0xFF == ord('k'):                      # Kalibrerar rutan så den kollar på rätt sak
         kal_top_left_rect, kal_bot_right_rect = CaliFrame(frame)
-        kalibrerad = CalibratedImage(frame,kal_top_left_rect, kal_bot_right_rect)
-        cv2.imshow('kalibrerad', kalibrerad)
+        Calibrated = CalibratedImage(frame,kal_top_left_rect, kal_bot_right_rect)
+        cv2.imshow('kalibrerad', Calibrated)
 
     elif cv2.waitKey(1) & 0xFF == ord("q"):                      # stänger ner programmet
         break
